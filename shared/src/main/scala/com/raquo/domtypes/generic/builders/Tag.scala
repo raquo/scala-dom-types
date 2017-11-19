7ef248e7b098ff107d5e21d561fcf12eb66d0dde
Nikita Gazarov <nikita@raquo.com>
2017-11-18 22:58:16 -0800

    API: Tag is now a class, remove Tag.build (see issue #12)

--- shared/src/main/scala/com/raquo/domtypes/generic/builders/Tag.scala
+++ shared/src/main/scala/com/raquo/domtypes/generic/builders/Tag.scala
@@ -1,14 +1,7 @@
 package com.raquo.domtypes.generic.builders
 
-/** Tag is an Element builder
-  *
-  * Note: we use `build` instead of `apply` because otherwise Scala compiler gets confused when using TagSyntax.apply in Scala DOM Builder
-  * */
-trait Tag[+Element] {
-
-  val tagName: String
-
+/** Tag represents an Element builder */
+class Tag[+Element](
+  val tagName: String,
   val void: Boolean
-
-  def build(): Element
-}
+)
