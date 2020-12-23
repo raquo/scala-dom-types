711149813ae566ef8552274d598e201ba38f53d2
Phuoc Nguyen <phuoc@huuphuoc.me>
2020-12-23 15:19:16 +0700

    New: Add animation events (#60)

--- js/src/main/scala/com/raquo/domtypes/jsdom/defs/package.scala
+++ js/src/main/scala/com/raquo/domtypes/jsdom/defs/package.scala
@@ -27,7 +27,7 @@ package object defs {
 
     type MediaEventProps[EP[_ <: dom.Event]] = generic.defs.eventProps.MediaEventProps[EP, dom.Event]
 
-    type MiscellaneousEventProps[EP[_ <: dom.Event]] = generic.defs.eventProps.MiscellaneousEventProps[EP, dom.Event, dom.UIEvent]
+    type MiscellaneousEventProps[EP[_ <: dom.Event]] = generic.defs.eventProps.MiscellaneousEventProps[EP, dom.Event, dom.AnimationEvent, dom.UIEvent]
 
     type MouseEventProps[EP[_ <: dom.Event]] = generic.defs.eventProps.MouseEventProps[
       EP,
--- shared/src/main/scala/com/raquo/domtypes/generic/defs/eventProps/MiscellaneousEventProps.scala
+++ shared/src/main/scala/com/raquo/domtypes/generic/defs/eventProps/MiscellaneousEventProps.scala
@@ -7,7 +7,44 @@ import com.raquo.domtypes.generic.builders.EventPropBuilder
   *
   * For type param docs see [[EventPropBuilder]]
   */
-trait MiscellaneousEventProps[EP[_ <: DomEvent], DomEvent, DomUIEvent <: DomEvent] { this: EventPropBuilder[EP, DomEvent] =>
+trait MiscellaneousEventProps[
+  EP[_ <: DomEvent],
+  DomEvent,
+  DomAnimationEvent <: DomEvent,
+  DomUIEvent <: DomEvent
+] { this: EventPropBuilder[EP, DomEvent] =>
+  /**
+    * An animationcancel event is sent when a CSS animation unexpectedly aborts, that is, any time it stops running
+    * without sending an animationend event. This can happen, for example, when the animation-name is changed such
+    * that the animation is removed, or when the animating node is hidden—either directly or because any of its
+    * containing nodes are hidden—using CSS.
+    *
+    * MDN
+    */
+  lazy val onAnimationCancel: EP[DomAnimationEvent] = eventProp("animationcancel")
+
+  /**
+    * The animationend event is event fires when a CSS animation reaches the end of its active period.
+    *
+    * MDN
+    */
+  lazy val onAnimationEnd: EP[DomAnimationEvent] = eventProp("animationend")
+
+  /**
+    * The animationiteration event is sent when a CSS animation reaches the end of an iteration. An iteration ends
+    * when a single pass through the sequence of animation instructions is completed by executing the last
+    * animation step.
+    *
+    * MDN
+    */
+  lazy val onAnimationIteration: EP[DomAnimationEvent] = eventProp("animationiteration")
+
+  /**
+    * The animationstart event is sent when a CSS Animation starts to play.
+    *
+    * MDN
+    */
+  lazy val onAnimationStart: EP[DomAnimationEvent] = eventProp("animationstart")
 
   /**
     * The onload property of the GlobalEventHandlers mixin is an event handler
