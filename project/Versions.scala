534a3f44f420d599fde73a9180cb29a0054b5c1a
Nikita Gazarov <nikita@raquo.com>
2021-01-21 21:33:55 -0800

    Build: Use Circle CI, remove github actions, travis, jitpack

---
+++ project/Versions.scala
@@ -0,0 +1,12 @@
+object Versions {
+
+  val Scala_2_11 = "2.11.12"
+
+  val Scala_2_13 = "2.13.4"
+
+  val Scala_2_12 = "2.12.12"
+
+  val Scala_3_RC1 = "3.0.0-RC1-bin-20210113-8345078-NIGHTLY"
+
+  val ScalaJsDom = "1.0.0"
+}
