7cf8cfca4338d21d03646126f950a3ce7ef91297
Nikita Gazarov <nikita@raquo.com>
2021-03-17 19:47:33 -0700

    New: AsIsCodec.apply convenience method

--- shared/src/main/scala/com/raquo/domtypes/generic/codecs/AsIsCodec.scala
+++ shared/src/main/scala/com/raquo/domtypes/generic/codecs/AsIsCodec.scala
@@ -5,3 +5,9 @@ trait AsIsCodec[T] extends Codec[T, T] {
   override def decode(domValue: T): T = domValue
   override def encode(scalaValue: T): T = scalaValue
 }
+
+object AsIsCodec {
+
+  /** Note: We already have several AsIsCodec instances in codecs/package.scala */
+  def apply[T]: AsIsCodec[T] = new AsIsCodec[T] {}
+}
