37b3abdf0ad8375a4a55b951a9bf8f3da9ae5776
busti <oss@busti.cool>
2021-09-15 22:21:28 +0200

    reformat SvgTagBuilder to be more in line with the other files

--- shared/src/main/scala/com/raquo/domtypes/generic/builders/SvgTagBuilder.scala
+++ shared/src/main/scala/com/raquo/domtypes/generic/builders/SvgTagBuilder.scala
@@ -3,20 +3,21 @@ package com.raquo.domtypes.generic.builders
 /** SvgTagBuilder is a builder of SVG Tags, which themselves are builders of SVG Elements. #factoryfactory */
 trait SvgTagBuilder[T[_ <: DomSvgElement], -DomSvgElement] {
 
-  type TForeignObject = T
-  @inline protected def svgForeignObjectTag[Ref <: DomSvgElement](tagName: String): TForeignObject[Ref] = {
-    svgTag(tagName)
-  }
-
-  type TSvg = T
-  @inline protected def svgSvgTag[Ref <: DomSvgElement](tagName: String): TSvg[Ref] = {
-    svgTag(tagName)
-  }
-
   type TPure = T
+  type TSvg = T
+  type TForeignObject = T
+
+  @inline protected def svgTag[Ref <: DomSvgElement](tagName: String, void: Boolean): TPure[Ref]
+
   @inline protected def svgTag[Ref <: DomSvgElement](tagName: String): TPure[Ref] = {
     svgTag(tagName, void = false)
   }
 
-  @inline protected def svgTag[Ref <: DomSvgElement](tagName: String, void: Boolean): TPure[Ref]
+  @inline protected def svgForeignObjectTag[Ref <: DomSvgElement](tagName: String): TForeignObject[Ref] = {
+    svgTag(tagName)
+  }
+
+  @inline protected def svgSvgTag[Ref <: DomSvgElement](tagName: String): TSvg[Ref] = {
+    svgTag(tagName)
+  }
 }
