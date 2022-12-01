4bcb89c00da04f31c5b93326e8b7611d8bfc1e08
Nikita Gazarov <nikita@raquo.com>
2022-12-01 14:54:25 -0800

    Test: StyleProp fixture now extends styles.traits.Base for global keywords

--- js/src/test/scala/com/thirdparty/keys/StyleProp.scala
+++ js/src/test/scala/com/thirdparty/keys/StyleProp.scala
@@ -1,12 +1,13 @@
 package com.thirdparty.keys
 
 import com.thirdparty.setters.StyleSetter
+import com.thirdparty.defs.styles
 
 import scala.scalajs.js.|
 
 case class StyleProp[V](
   domName: String
-) extends DerivedStylePropBuilder[DerivedStyleProp] {
+) extends DerivedStylePropBuilder[DerivedStyleProp] with styles.traits.Base {
 
   def := (value: V | String): StyleSetter[V] = StyleSetter(this, value.toString)
 
