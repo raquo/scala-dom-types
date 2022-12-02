51145f4ca46bb3dbc2e0ce84ec86dfb577f399a2
Nikita Gazarov <nikita@raquo.com>
2022-12-01 21:20:42 -0800

    API: Remove temporary parser and generator used to convert old SDT traits into new SDT data structures

--- shared/src/main/scala/com/raquo/domtypes/temp/parsers/package.scala
+++
@@ -1,9 +0,0 @@
-package com.raquo.domtypes.temp
-
-import scala.language.implicitConversions
-
-package object parsers {
-
-  implicit def stringToParsable(str: String): Parsable = new Parsable(str)
-
-}
