4553cdd3e4b8ae31c134baecf161ee7d4f7cf896
Nikita Gazarov <nikita@raquo.com>
2022-12-01 21:57:41 -0800

    Mix: Remove unused files

--- shared/src/main/scala/com/raquo/domtypes/common/StyleUnitDef.scala
+++
@@ -1,9 +0,0 @@
-package com.raquo.domtypes.common
-
-case class StyleUnitDef(
-  scalaName: String,
-  valueType: String,
-  encoderImpl: String,
-  override val commentLines: List[String],
-  override val docUrls: List[String]
-) extends KeyDef
