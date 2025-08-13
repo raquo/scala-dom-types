251495505cf7a536d0c1926ba63b2a4e1806c002
Simon Parten <Quafadas@users.noreply.github.com>
2025-08-13 08:44:23 +0000

    .

--- js/src/test/scala/com/thirdparty/defs/attrs/MathMLAttrs.scala
+++ js/src/test/scala/com/thirdparty/defs/attrs/MathMLAttrs.scala
@@ -23,34 +23,88 @@ trait MathMLAttrs {
 
 
   /**
-    * This enumerated attribute specifies how the enclosed MathML markup should be rendered.
-    * It can have one of the following values:
-    * - block, which means that this element will be displayed in its own block outside the current span of text
-    * - inline, which means that this element will be displayed inside the current span of text
+    * A Boolean indicating whether the operator should be treated as an accent
+    * when used as an under- or over-script.
     * 
-    * If not present, its default value is inline.
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mo#accent mo#accent @ MDN]]
+    */
+  lazy val accent: MathMLAttr[Boolean] = boolAsPresenceMathMLAttr("accent")
+
+
+  /**
+    * A Boolean indicating whether the under script should be treated as an accent.
     * 
-    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Reference/Attribute Attribute @ MDN]]
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/munder#accentunder munder#accentunder @ MDN]]
+    */
+  lazy val accentunder: MathMLAttr[Boolean] = boolAsPresenceMathMLAttr("accentunder")
+
+
+  /**
+    * Specifies vertical alignment of the table with respect to its environment.
+    * 
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mtable#align mtable#align @ MDN]]
+    */
+  lazy val align: MathMLAttr[String] = stringMathMLAttr("align")
+
+
+  /**
+    * Specifies the horizontal alignment of table cells.
+    * 
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mtable#columnalign mtable#columnalign @ MDN]]
+    */
+  lazy val columnalign: MathMLAttr[String] = stringMathMLAttr("columnalign")
+
+
+  /**
+    * Specifies table column borders.
+    * 
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mtable#columnlines mtable#columnlines @ MDN]]
+    */
+  lazy val columnlines: MathMLAttr[String] = stringMathMLAttr("columnlines")
+
+
+  /**
+    * Specifies the space between table columns.
+    * 
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mtable#columnspacing mtable#columnspacing @ MDN]]
+    */
+  lazy val columnspacing: MathMLAttr[String] = stringMathMLAttr("columnspacing")
+
+
+  /**
+    * A non-negative integer value that indicates over how many table columns the cell extends.
+    * 
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mtd#columnspan mtd#columnspan @ MDN]]
+    */
+  lazy val columnspan: MathMLAttr[Int] = intMathMLAttr("columnspan")
+
+
+  /**
+    * A length-percentage indicating the desired depth (below the baseline).
+    * 
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mpadded#depth mpadded#depth @ MDN]]
+    */
+  lazy val depth: MathMLAttr[String] = stringMathMLAttr("depth")
+
+
+  /**
+    * The text direction. Possible values are either ltr (left to right) or rtl (right to left).
+    * 
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Global_attributes/dir dir @ MDN]]
+    */
+  lazy val dir: MathMLAttr[String] = stringMathMLAttr("dir")
+
+
+  /**
+    * Specifies the rendering mode. The values block and inline are allowed.
+    * 
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/math#display math#display @ MDN]]
     */
   lazy val display: MathMLAttr[String] = stringMathMLAttr("display")
 
 
   /**
-    * This attribute specifies the logical class of the identifier, which varies
-    * in typography. That is, although the names suggest the typographic style for
-    * the class, semantically, items with the same class are treated "the same"
-    * within an expression, which might or might not involve displaying them with
-    * the same typography.
-    * 
-    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Reference/Attribute Attribute @ MDN]]
-    */
-  lazy val mathvariant: MathMLAttr[String] = stringMathMLAttr("mathvariant")
-
-
-  /**
-    * This Boolean attribute specifies whether more vertical space is used
-    * for displayed equations or, if set to false, a more compact layout
-    * is used to display formulas.
+    * A Boolean specifying whether to set the math-style to normal (if true) or compact (otherwise).
     * 
     * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Global_attributes/displaystyle displaystyle @ MDN]]
     */
@@ -58,54 +112,8 @@ trait MathMLAttrs {
 
 
   /**
-    * This attribute specifies a scriptlevel for the element. Larger values
-    * mean smaller font sizes. The scriptlevel is used to control the font size
-    * in a context-dependent way.
-    * 
-    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Global_attributes/scriptlevel scriptlevel @ MDN]]
-    */
-  lazy val scriptlevel: MathMLAttr[Int] = intMathMLAttr("scriptlevel")
-
-
-  /**
-    * This attribute specifies the thickness of the horizontal fraction line.
-    * Possible values: a length value like '2px', 'thin', 'medium', 'thick', or 0 for no line.
-    * 
-    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mfrac#linethickness mfrac#linethickness @ MDN]]
-    */
-  lazy val linethickness: MathMLAttr[String] = stringMathMLAttr("linethickness")
-
-
-  /**
-    * This attribute specifies the alignment of the numerator over the fraction line.
-    * Possible values are: left, center (default), and right.
-    * 
-    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mfrac#numalign mfrac#numalign @ MDN]]
-    */
-  lazy val numalign: MathMLAttr[String] = stringMathMLAttr("numalign")
-
-
-  /**
-    * This attribute specifies the alignment of the denominator under the fraction line.
-    * Possible values are: left, center (default), and right.
-    * 
-    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mfrac#denomalign mfrac#denomalign @ MDN]]
-    */
-  lazy val denomalign: MathMLAttr[String] = stringMathMLAttr("denomalign")
-
-
-  /**
-    * This Boolean attribute specifies whether the operator is a separator
-    * (such as commas). There is no visual effect for this attribute.
-    * 
-    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mo#separator mo#separator @ MDN]]
-    */
-  lazy val separator: MathMLAttr[Boolean] = boolAsPresenceMathMLAttr("separator")
-
-
-  /**
-    * This Boolean attribute specifies whether the operator is a fence
-    * (such as parentheses). There is no visual effect for this attribute.
+    * A Boolean specifying whether the operator is a fence (such as parentheses).
+    * There is no visual effect for this attribute.
     * 
     * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mo#fence mo#fence @ MDN]]
     */
@@ -113,44 +121,55 @@ trait MathMLAttrs {
 
 
   /**
-    * This Boolean attribute specifies whether the operator stretches to the
-    * size of the adjacent element.
+    * Specifies borders of an entire mtable. Possible values are: none (default), solid and dashed.
     * 
-    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mo#stretchy mo#stretchy @ MDN]]
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mtable#frame mtable#frame @ MDN]]
     */
-  lazy val stretchy: MathMLAttr[Boolean] = boolAsPresenceMathMLAttr("stretchy")
+  lazy val frame: MathMLAttr[String] = stringMathMLAttr("frame")
 
 
   /**
-    * This Boolean attribute specifies whether a stretchy operator should be
-    * vertically symmetric around the imaginary math axis (centered fraction line).
+    * Specifies additional space added between the table and frame.
     * 
-    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mo#symmetric mo#symmetric @ MDN]]
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mtable#framespacing mtable#framespacing @ MDN]]
     */
-  lazy val symmetric: MathMLAttr[Boolean] = boolAsPresenceMathMLAttr("symmetric")
+  lazy val framespacing: MathMLAttr[String] = stringMathMLAttr("framespacing")
 
 
   /**
-    * This Boolean attribute specifies whether the operator should be drawn larger
-    * than normal when displaystyle is true.
+    * A length-percentage indicating the desired height (above the baseline).
     * 
-    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mo#largeop mo#largeop @ MDN]]
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mpadded#height mpadded#height @ MDN]]
     */
-  lazy val largeop: MathMLAttr[Boolean] = boolAsPresenceMathMLAttr("largeop")
+  lazy val height: MathMLAttr[String] = stringMathMLAttr("height")
 
 
   /**
-    * This Boolean attribute specifies whether attached under- and overscripts
-    * move to sub- and superscript positions when displaystyle is false.
+    * Used to set a hyperlink to a specified URI.
     * 
-    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mo#movablelimits mo#movablelimits @ MDN]]
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Global_attributes/href href @ MDN]]
     */
-  lazy val movablelimits: MathMLAttr[Boolean] = boolAsPresenceMathMLAttr("movablelimits")
+  lazy val href: MathMLAttr[String] = stringMathMLAttr("href")
 
 
   /**
-    * This attribute specifies the amount of space before the operator.
-    * Values can be length values or named space constants like 'thickmathspace'.
+    * Sets up a unique identifier associated with the element.
+    * 
+    * [[https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/id id @ MDN]]
+    */
+  lazy val id: MathMLAttr[String] = stringMathMLAttr("id")
+
+
+  /**
+    * A length-percentage indicating the thickness of the horizontal fraction line.
+    * 
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mfrac#linethickness mfrac#linethickness @ MDN]]
+    */
+  lazy val linethickness: MathMLAttr[String] = stringMathMLAttr("linethickness")
+
+
+  /**
+    * A length-percentage indicating amount of space before the operator.
     * 
     * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mo#lspace mo#lspace @ MDN]]
     */
@@ -158,8 +177,104 @@ trait MathMLAttrs {
 
 
   /**
-    * This attribute specifies the amount of space after the operator.
-    * Values can be length values or named space constants like 'thickmathspace'.
+    * A background-color for the element.
+    * 
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Global_attributes/mathbackground mathbackground @ MDN]]
+    */
+  lazy val mathbackground: MathMLAttr[String] = stringMathMLAttr("mathbackground")
+
+
+  /**
+    * A color for the element.
+    * 
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Global_attributes/mathcolor mathcolor @ MDN]]
+    */
+  lazy val mathcolor: MathMLAttr[String] = stringMathMLAttr("mathcolor")
+
+
+  /**
+    * A length-percentage used as a font-size for the element.
+    * 
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Global_attributes/mathsize mathsize @ MDN]]
+    */
+  lazy val mathsize: MathMLAttr[String] = stringMathMLAttr("mathsize")
+
+
+  /**
+    * The logical class of token elements, which varies in typography.
+    * 
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Global_attributes/mathvariant mathvariant @ MDN]]
+    */
+  lazy val mathvariant: MathMLAttr[String] = stringMathMLAttr("mathvariant")
+
+
+  /**
+    * A length-percentage indicating the maximum size of the operator when it is stretchy.
+    * 
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mo#maxsize mo#maxsize @ MDN]]
+    */
+  lazy val maxsize: MathMLAttr[String] = stringMathMLAttr("maxsize")
+
+
+  /**
+    * A length-percentage indicating the minimum size of the operator when it is stretchy.
+    * 
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mo#minsize mo#minsize @ MDN]]
+    */
+  lazy val minsize: MathMLAttr[String] = stringMathMLAttr("minsize")
+
+
+  /**
+    * A Boolean indicating whether attached under- and overscripts move to sub-
+    * and superscript positions when math-style is set to compact.
+    * 
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mo#movablelimits mo#movablelimits @ MDN]]
+    */
+  lazy val movablelimits: MathMLAttr[Boolean] = boolAsPresenceMathMLAttr("movablelimits")
+
+
+  /**
+    * A list of notations, separated by white space, to apply to the child elements.
+    * 
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/menclose#notation menclose#notation @ MDN]]
+    */
+  lazy val notation: MathMLAttr[String] = stringMathMLAttr("notation")
+
+
+  /**
+    * Specifies the vertical alignment of table cells.
+    * 
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mtable#rowalign mtable#rowalign @ MDN]]
+    */
+  lazy val rowalign: MathMLAttr[String] = stringMathMLAttr("rowalign")
+
+
+  /**
+    * Specifies table row borders.
+    * 
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mtable#rowlines mtable#rowlines @ MDN]]
+    */
+  lazy val rowlines: MathMLAttr[String] = stringMathMLAttr("rowlines")
+
+
+  /**
+    * Specifies the space between table rows.
+    * 
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mtable#rowspacing mtable#rowspacing @ MDN]]
+    */
+  lazy val rowspacing: MathMLAttr[String] = stringMathMLAttr("rowspacing")
+
+
+  /**
+    * A non-negative integer value that indicates on how many rows does the cell extend.
+    * 
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mtd#rowspan mtd#rowspan @ MDN]]
+    */
+  lazy val rowspan: MathMLAttr[Int] = intMathMLAttr("rowspan")
+
+
+  /**
+    * A length-percentage indicating the amount of space after the operator.
     * 
     * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mo#rspace mo#rspace @ MDN]]
     */
@@ -167,36 +282,60 @@ trait MathMLAttrs {
 
 
   /**
-    * This attribute specifies the desired width. See length for possible values.
+    * Specifies a math-depth for the element. See the scriptlevel page for accepted values and mapping.
     * 
-    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mspace#width mspace#width @ MDN]]
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Global_attributes/scriptlevel scriptlevel @ MDN]]
+    */
+  lazy val scriptlevel: MathMLAttr[Int] = intMathMLAttr("scriptlevel")
+
+
+  /**
+    * A Boolean specifying whether the operator is a separator (such as commas).
+    * There is no visual effect for this attribute.
+    * 
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mo#separator mo#separator @ MDN]]
+    */
+  lazy val separator: MathMLAttr[Boolean] = boolAsPresenceMathMLAttr("separator")
+
+
+  /**
+    * A Boolean indicating whether the operator stretches to the size of the adjacent element.
+    * 
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mo#stretchy mo#stretchy @ MDN]]
+    */
+  lazy val stretchy: MathMLAttr[Boolean] = boolAsPresenceMathMLAttr("stretchy")
+
+
+  /**
+    * A Boolean indicating whether a stretchy operator should be vertically symmetric
+    * around the imaginary math axis (centered fraction line).
+    * 
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mo#symmetric mo#symmetric @ MDN]]
+    */
+  lazy val symmetric: MathMLAttr[Boolean] = boolAsPresenceMathMLAttr("symmetric")
+
+
+  /**
+    * A length-percentage indicating the vertical location of the positioning point
+    * of the child content with respect to the positioning point of the element.
+    * 
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mpadded#voffset mpadded#voffset @ MDN]]
+    */
+  lazy val voffset: MathMLAttr[String] = stringMathMLAttr("voffset")
+
+
+  /**
+    * A length-percentage indicating the desired width.
+    * 
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mpadded#width mpadded#width @ MDN]]
     */
   lazy val width: MathMLAttr[String] = stringMathMLAttr("width")
 
 
   /**
-    * This attribute specifies the desired height above the baseline.
-    * See length for possible values.
+    * Specifies the URI for the MathML namespace (http://www.w3.org/1998/Math/MathML).
     * 
-    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mspace#height mspace#height @ MDN]]
-    */
-  lazy val height: MathMLAttr[String] = stringMathMLAttr("height")
-
-
-  /**
-    * This attribute specifies the desired depth below the baseline.
-    * See length for possible values.
-    * 
-    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/mspace#depth mspace#depth @ MDN]]
-    */
-  lazy val depth: MathMLAttr[String] = stringMathMLAttr("depth")
-
-
-  /**
-    * The xmlns attribute specifies the XML namespace for the MathML element.
-    * For MathML, this should typically be 'http://www.w3.org/1998/Math/MathML'.
-    * 
-    * [[https://www.w3schools.com/tags/att_html_xmlns.asp]]
+    * [[https://developer.mozilla.org/en-US/docs/Web/MathML/Element/math#xmlns math#xmlns @ MDN]]
     */
   lazy val xmlns: MathMLAttr[String] = stringMathMLAttr("xmlns")
 
