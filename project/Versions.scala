19e2f65e1db2716323d55e0633aac46356ffe324
Nikita Gazarov <nikita@raquo.com>
2026-09-19 17:59:55 -0700

    Build: Bump versions

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,7 +4,7 @@ object Versions {
 
   val Scala_2_13 = "2.13.18"
 
-  val Scala_3 = "3.3.7"
+  val Scala_3 = "3.9.0"
 
   // -- Dependencies --
 
@@ -12,7 +12,7 @@ object Versions {
 
   // -- Test --
 
-  val ScalaTest = "3.2.19"
+  val ScalaTest = "3.2.20"
 
-  val Ew = "0.3.0"
+  val Ew = "0.3.1"
 }
